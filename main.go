a7cf8998e5b108eacc8146cef47df70389d853f3
Edjan Michiles <esvm@cin.ufpe.br>
2019-06-12 01:32:47 -0300

    fix build

--- main.go
+++ main.go
@@ -1,55 +1,33 @@
-package main
+// package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/esvm/dcm-middleware/dcm"
-	"github.com/esvm/dcm-middleware/distribution/models"
-	"github.com/esvm/dcm-middleware/scylla"
-	log "github.com/sirupsen/logrus"
-)
+// 	"github.com/esvm/dcm-middleware/dcm"
+// 	"github.com/esvm/dcm-middleware/distribution/models"
+// 	"github.com/esvm/dcm-middleware/scylla"
+// 	log "github.com/sirupsen/logrus"
+// )
 
-func main() {
-	c, err := dcm.Connect("localhost", 8426, 100)
-	defer c.Close()
-	topic, err := c.CreateTopic("Edjan5", models.TopicProperties{IndexName: "xalala", StartFrom: scylla.Begin})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+// func main() {
+// 	c, err := dcm.Connect("localhost", 8426, 100)
+// 	defer c.Close()
+// 	topic, err := c.CreateTopic("Edjan5", models.TopicProperties{IndexName: "xalala", StartFrom: scylla.Begin})
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
 
-	consume(c, topic)
-}
+// 	consume(c, topic)
+// }
 
-func publish(c *dcm.Connection, topic *models.Topic) {
-	message := &models.Message{
-		TopicID: topic.ID,
-		Payload: 1.5,
-	}
+// func consume(c *dcm.Connection, topic *models.Topic) {
+// 	ch, err := c.Consume(topic.ID, topic.Properties.IndexName)
+// 	if err != nil {
+// 		log.Debug(err)
+// 	}
 
-	err := c.Publish(topic.ID, message)
-	if err != nil {
-		log.Debug(err)
-	}
-
-	message = &models.Message{
-		TopicID: topic.ID,
-		Payload: 4.7,
-	}
-
-	err = c.Publish(topic.ID, message)
-	if err != nil {
-		log.Debug(err)
-	}
-}
-
-func consume(c *dcm.Connection, topic *models.Topic) {
-	ch, err := c.Consume(topic.ID, topic.Properties.IndexName)
-	if err != nil {
-		log.Debug(err)
-	}
-
-	for metric := range ch {
-		log.Info(metric)
-	}
-}
+// 	for metric := range ch {
+// 		log.Info(metric)
+// 	}
+// }
--- publisher.go
+++ publisher.go
@@ -1,62 +1,44 @@
-package main
+// package main
 
-import (
-	"encoding/gob"
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/esvm/dcm-middleware/dcm"
-	"github.com/esvm/dcm-middleware/distribution/models"
-	"github.com/esvm/dcm-middleware/scylla"
-	log "github.com/sirupsen/logrus"
-)
+// 	"github.com/esvm/dcm-middleware/dcm"
+// 	"github.com/esvm/dcm-middleware/distribution/models"
+// 	"github.com/esvm/dcm-middleware/scylla"
+// 	log "github.com/sirupsen/logrus"
+// )
 
-func initLog() {
-	log.SetLevel(log.DebugLevel)
-	log.SetFormatter(&log.TextFormatter{})
-}
+// func main() {
+// 	c, err := dcm.Connect("localhost", 8426, 100)
+// 	defer c.Close()
+// 	topic, err := c.CreateTopic("Edjan5", models.TopicProperties{IndexName: "xalala", StartFrom: scylla.Begin})
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
 
-func register() {
-	gob.Register(&models.CreateTopicRequest{})
-	gob.Register(&models.CreateTopicResponse{})
-	gob.Register(&models.PublishRequest{})
-	gob.Register(&models.PublishResponse{})
-	gob.Register(&models.ConsumeRequest{})
-	gob.Register(&models.ConsumeResponse{})
-}
+// 	publish(c, topic)
+// }
 
-func main() {
-	initLog()
-	register()
+// func publish(c *dcm.Connection, topic *models.Topic) {
+// 	message := &models.Message{
+// 		TopicID: topic.ID,
+// 		Payload: 1.5,
+// 	}
 
-	c, err := dcm.Connect("localhost", 8426)
-	defer c.Close()
-	topic, err := c.CreateTopic("Edjan5", models.TopicProperties{IndexName: "xalala", StartFrom: scylla.Begin})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+// 	err := c.Publish(topic.ID, message)
+// 	if err != nil {
+// 		log.Debug(err)
+// 	}
 
-	publish(c, topic)
-}
+// 	message = &models.Message{
+// 		TopicID: topic.ID,
+// 		Payload: 4.7,
+// 	}
 
-func publish(c *dcm.Connection, topic *models.Topic) {
-	message := &models.Message{
-		TopicID: topic.ID,
-		Payload: 1.5,
-	}
-
-	err := c.Publish(topic.ID, message)
-	if err != nil {
-		log.Debug(err)
-	}
-
-	message = &models.Message{
-		TopicID: topic.ID,
-		Payload: 4.7,
-	}
-
-	err = c.Publish(topic.ID, message)
-	if err != nil {
-		log.Debug(err)
-	}
-}
+// 	err = c.Publish(topic.ID, message)
+// 	if err != nil {
+// 		log.Debug(err)
+// 	}
+// }
